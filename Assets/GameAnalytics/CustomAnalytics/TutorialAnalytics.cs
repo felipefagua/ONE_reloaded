441851446b1d3c59ab50755bcfe5317effa4c4c4
Jorge Ferro <jferrov93@gmail.com>
2017-09-20 12:42:23 -0500

    fix analytics tutorial

---
+++ Assets/GameAnalytics/CustomAnalytics/TutorialAnalytics.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialAnalytics : MonoBehaviour, IGameAnalytics {
+	//Tutorial analytics empty
+
+	public void ExitSession ()
+	{
+		
+	}
+
+	public void GameOver ()
+	{
+	}
+	public void GainedALife ()
+	{
+	}
+}
